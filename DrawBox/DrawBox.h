2e156d5dd209349b48facdad7e1b27978f6c9071
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2022-02-18 11:36:28 -0800

    Initial Commit

---
+++ DrawBox/DrawBox.h
@@ -0,0 +1,18 @@
+//
+//  DrawBox.h
+//  DrawBox
+//
+//  Created by Jenya Lebid on 2/18/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DrawBox.
+FOUNDATION_EXPORT double DrawBoxVersionNumber;
+
+//! Project version string for DrawBox.
+FOUNDATION_EXPORT const unsigned char DrawBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawBox/PublicHeader.h>
+
+
